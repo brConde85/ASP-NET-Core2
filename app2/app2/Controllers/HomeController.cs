f2a8a3a07486b51debffe920b33049771413c36b
Bruno Luiz Panho Marciano <55405814+brConde85@users.noreply.github.com>
2020-03-15 20:04:43 -0300

    Adição de novas telas e métodos para exibição da lista de convidados para festa que confirmaram presença.

--- app2/app2/Controllers/HomeController.cs
+++ app2/app2/Controllers/HomeController.cs
@@ -22,11 +22,30 @@ namespace app2.Controllers
         {
             return View("inicio");
         }
-        
+        [HttpGet] //Permite a passagem de parâmetros via URL, REQUEST PARA CLICAR EM PAGINA DE INTERNET
         public ViewResult Formulario()
         {
             return View("formulario");
         }
 
+        // Haverá uma submissão entre os dois métodos onde o formlário que será executado será o HttpPost
+
+        [HttpPost] // Para validação de um formulário
+        //ViewResult para devolver uma nova view contendo os resultados do formulário        
+        // Foi realizada a inserção de um parâmetro onde será injetado em formulario o conjunto das respostas informadas pelo usuário em Models: RepostaConvidados 
+        public ViewResult Formulario(RespostaConvidados resposta)
+        {
+            //chamando classe estática repositorio de model para utilizar o método AdicionarResposta
+            //para utilizarmos o formulario depois que ele for submetido
+            Repositorio.AdicionarResposta(resposta);// o objeto resposta vem do formulario apos ele ser preenchido
+            //Retorno de uma tela de agradecimento
+            return View("Obrigado", resposta);
+        }
+
+        public ViewResult ListaConvidados()
+        {
+            // retornando uma lista completa com a a condicional where mostrando apenas os convidados que confirmaram a presença
+            return View("ListaFinal",Repositorio.ListaFinal.Where(c => c.Presente == true));
+        }
     }
 }
---
+++ app2/app2/Models/Repositorio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app2.Models
+{
+    // Definimos a classe como estática para podermos guardar uma informção temporariamente em uma lista
+    // obs: foi realizado dessa forma pois ainda não foi criado um banco para guardar essas informações permanentemente
+    public static class Repositorio
+    {
+        // Assim que a aplicação estiver sendo executada senda a classe static será feita a instanciação da LISTA
+        // Além da lista ser iniciada de forma vazia
+        private static List<RespostaConvidados> LISTA = new List<RespostaConvidados>();
+
+        // Método para criação de convidados para lista de convidados
+        public static void AdicionarResposta(RespostaConvidados resposta)// esse metodo permite adicionar a lista um objeto do tipo RespostaConvidados
+        {
+            LISTA.Add(resposta);
+        }
+
+        // Método para buscar  as Respostas do usuário
+        // uma maneira de encapsular a lista
+        public static IEnumerable<RespostaConvidados>ListaFinal //OBS: IEnumerable PARA CRIAR UMA COLEÇÃO QUE POSSA SER OBJETO DE UM FOR-IT
+        {
+            // GET para devolver um objeto do tipo: IEnumerable<RespostaConvidados>
+            get
+            {
+                return LISTA;
+            }
+        }
+    }
+}
