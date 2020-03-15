826284e185c5d9b0f997ce7e7a08747ca88ea66a
Bruno Luiz Panho Marciano <55405814+brConde85@users.noreply.github.com>
2020-03-15 13:20:50 -0300

    Inserção do llink de interligação entre uma pagina e outra

---
+++ app2/app2/Models/RespostaConvidados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app2.Models
+{
+    public class RespostaConvidados
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public bool? Presente { get; set; } 
+        // bool com ponto de interrogação permite um terceiro valor:
+        //"first->true, second-> false and third-> null."
+
+        
+    }
+}
---
+++ asp1/asp1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using asp1.Models;
+
+namespace asp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index()
+        {
+            ViewBag.Nome = "Bruno";
+            ViewBag.SobreNome = "Luiz Panho Marciano";
+            ViewBag.Idade = 34;
+            ViewBag.Sexo = "M";
+            ViewBag.EstCiv = "Divórciado";
+            ViewBag.Bool = true;
+            return View("Index");
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
