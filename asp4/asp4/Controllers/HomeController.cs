0653374aad7917f8810f85384444e9f8a0c3fd1d
Bruno Lpm <bruno_lpm@hotmail.com>
2020-03-16 14:22:22 -0300

    add novos projetos

---
+++ asp4/asp4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp4.ViewModels;
+
+namespace asp4.Controllers
+{
+    public class HomeController : Controller
+    {
+        Cliente novo = new Cliente()
+        {
+            nome = "Bruno",
+            sobreNome = "Marciano",
+            telefone = "123456"
+
+        };
+        public IActionResult Index()
+        {           
+            return View(novo);
+        }
+    }
+}
---
+++ asp4/asp4/ViewModels/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp4.ViewModels
+{
+    public class Cliente
+    {
+        public string nome { get; set; }
+        public string sobreNome { get; set; }
+        public string telefone { get; set; }
+
+    }
+}
