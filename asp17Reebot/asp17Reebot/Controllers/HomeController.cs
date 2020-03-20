d32e947b72e1540412287ea5fdcaac9637bbdd20
Bruno Lpm <bruno_lpm@hotmail.com>
2020-03-20 16:55:59 -0300

    asp17 (Novo) forma mais tranquila de entender as relações entre a estrutura MVC

---
+++ asp17Reebot/asp17Reebot/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp17Reebot.Models;
+
+namespace asp17Reebot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //para passarmos um unico cliente podemos utlizar essa estrutura
+            Clientes novoCliente = new Clientes();
+            novoCliente.Nome = "Bruno";
+            novoCliente.Sobrenome = "Marciano";
+
+            return View(novoCliente);
+        }
+    }
+}
