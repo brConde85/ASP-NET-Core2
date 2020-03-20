f7824fcd10f822c4bc90758a229f0d2bed519b8a
Bruno Lpm <bruno_lpm@hotmail.com>
2020-03-20 15:56:58 -0300

    Asp17 Iniciando projetos utilizando a classe Model, realizando a condtrução de Listas: List<Clientes> ListaClientes = new List<Clientes>();

---
+++ asp17/asp17/Controlles/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp17.Models;
+
+namespace asp17.Controlles
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Clientes> ListaClientes = new List<Clientes>();
+            ListaClientes.Add(new Clientes() { Nome = "Joao",  Sobrenome = "Ribeiro" });
+            ListaClientes.Add(new Clientes() { Nome = "Bruno", Sobrenome = "Marciano" });
+            ListaClientes.Add(new Clientes() { Nome = "Carlos", Sobrenome = "Eduardo" });
+            ListaClientes.Add(new Clientes() { Nome = "Ana", Sobrenome = "Flávia" });
+
+            return View(ListaClientes);
+        }
+    }
+}
