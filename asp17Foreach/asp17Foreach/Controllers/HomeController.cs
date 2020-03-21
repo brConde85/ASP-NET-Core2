a9bb22fd715f7ce3a80c007bdfe992de88545c40
Bruno Luiz Panho Marciano <55405814+brConde85@users.noreply.github.com>
2020-03-21 20:04:05 -0300

    adicionado metodo forech para interar uma lista de clientes

---
+++ asp17Foreach/asp17Foreach/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp17Foreach.Models;
+
+namespace asp17Foreach.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Definição da lista de dados
+        List<Clientes> ListaCliente;
+        // Criação de um método construtor para homeController
+        public HomeController()
+        {
+            ListaCliente = new List<Clientes>()
+            {
+                new Clientes(){ Id = 0, Nome = "Bruno", SobreNome = "Marciano"},
+                new Clientes(){ Id = 1, Nome = "Bruna", SobreNome = "Coelho"},
+                new Clientes(){ Id = 2, Nome = "Leonardo", SobreNome = "Granola"},
+                new Clientes(){ Id = 3, Nome = "Loire", SobreNome = "Carmem"},
+                new Clientes(){ Id = 4, Nome = "Abner", SobreNome = "Marciano"}
+            };
+
+        }
+        public IActionResult Index()
+        {                
+            return View(ListaCliente);
+        }
+        public ViewResult Detalhes(int id)
+        {
+            return View(ListaCliente[id]);
+        }
+    }
+}
