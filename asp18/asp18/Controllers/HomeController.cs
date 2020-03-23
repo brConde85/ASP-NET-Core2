4502b9962ee5d9de3a327e40c2a274bbd5e30af7
Bruno Luiz Panho Marciano <55405814+brConde85@users.noreply.github.com>
2020-03-22 21:34:20 -0300

    asp18 com adição de elementos do MVC conténdo Edição de dados e Deletar dados.

---
+++ asp18/asp18/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using asp18.Models;
+
+namespace asp18.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // view de entrada que apresente um titulo, a lista de clientes ou a mensagem 
+            //indicando que não existem clientes
+
+            return View(Dados.TodosOsClientes());
+        }
+
+        // Criação de dois métodos uma para apresentar um formulário e o outro para pocessar os dados que vem do formulário
+        public IActionResult NovoCliente()
+        {
+            // Apresenta um formulário para um novo cliente 
+            return View();
+        }
+        [HttpPost]
+        // Metodo para redirecionar para ação a Index
+        public IActionResult NovoCliente(Cliente item)
+        {
+            //Pede ao Model que adicione um novo cliente para a coleção... caso seja possivel(caso o cliente não exista no sistema)
+            Dados.AdicionarCliente(item);
+            // Esse metodo faz com que ao salvar um novo cliente nos iremos retornar a Index principal
+            return RedirectToAction("Index");
+        }
+        
+        // formulário está relacionada com o model
+        public IActionResult Apagar(int id)
+        {
+            // realizar uma requisição ao model solicitando a ação de deletar o cliente informado
+            Dados.ApagarCliente(id);
+
+            return RedirectToAction("Index");
+        }
+        //Metodo para Update do cliente carregando todos os seus dados para atualização 
+        public IActionResult Editar(int id)
+        {
+            // apresentar um aview
+            return View(Dados.DadosCliente(id));
+        }
+        [HttpPost]
+        //Criado para alterar os dados do cliente
+        public IActionResult Editar(Cliente c)
+        {
+            Dados.EditarCliente(c);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ asp18/asp18/Models/Dados.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp18.Models
+{
+    // Criada para simular um database
+    public static class Dados
+    {
+        //para praticar criamos uma lista estatica simulando um database
+        private static List<Cliente> ListaCliente = new List<Cliente>();
+
+        //metodo para termos acesso aos dados da lista de clientes (IEnumerable)     
+        public static IEnumerable<Cliente> TodosOsClientes()
+        {
+            return ListaCliente;
+        }
+        // metodo para adicionar uma novo Cliente
+        public static void AdicionarCliente(Cliente item)
+        {
+            // Atribuir Id ao item
+            /*
+             * item.Id
+             * item.Nome
+             * item.Telefone
+             */
+            item.Id = 0;
+            if (ListaCliente.Count != 0)
+            {
+                // LAST para devolver o ultimo elemento de uma sequencia
+                item.Id = ListaCliente.Last<Cliente>().Id + 1;/* essa expressão captura o ultimo id adicionado
+                e incrementa mais um, para evitar problemas de duplicidade de id.*/
+            }
+            ListaCliente.Add(item);
+        }
+        //metodo para deletar um cliente
+        public static void ApagarCliente(int id)
+        {
+            // Solicitando o primeiro cliente que esteja dentro da seguite expressão
+            var item = ListaCliente.First<Cliente>(i => i.Id == id); // id precisa ser igual ao id fornecido
+            ListaCliente.Remove(item);
+        }
+
+        // metodo para pegar os dados de um cliente espcifico e devolve para ao controller para usa atualização
+        public static Cliente DadosCliente(int id)
+        {
+            // Selecionar de uma lista o id que é igual ao id informado
+            var cliente = ListaCliente.Where(i => i.Id == id).FirstOrDefault();
+            return cliente;
+        }
+        // Metodo para edtar clientes(Utilizando a linguagem link)
+        public static void EditarCliente(Cliente c)
+        {
+            // Primeiro devemos buscar o cliente selecionado e depois informar o novo estado dele
+            ListaCliente.Where(i => i.Id == c.Id).First().Nome = c.Nome;//Com isso pegamos os valores do clinte C e bustituimos apos comparação 
+            ListaCliente.Where(i => i.Id == c.Id).First().Telefone = c.Telefone;
+        }
+
+    }
+}
