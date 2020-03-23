fee1af0a8be3fb5fe82fc62dc0377d36467b28b1
Bruno Lpm <bruno_lpm@hotmail.com>
2020-03-23 17:20:03 -0300

    asp19 projeto quase finalizado com inserção de dados do cliente  para lista estatica simulando uma database. Apresentação dos dados em uma tabela.

--- asp19/asp19/Controllers/HomeController.cs
+++ asp19/asp19/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using asp19.Models;
 
 namespace asp19.Controllers
 {
@@ -10,7 +11,26 @@ namespace asp19.Controllers
     {
         public IActionResult Index()
         {
+            //Apresneta a menu inicial
             return View();
+        }
+        public IActionResult ListaClientes()
+        {
+            //Apresenta a lista de Clientes da aplicação
+            return View(Dados.TodosClientes());
+        }
+        public IActionResult AdicionarCliente()
+        {
+            //Adicionar novo cliente
+            return View();
+        }
+        // metodo para guardar o cliente adicionado na view para inserção no Model
+        [HttpPost]
+        public IActionResult AdicionarCliente(Cliente cliente)
+        {   //depois dos dados tratados pela variavel será carregado o mesmo formulario para adição de novos clientes 
+            //guardar os dados do nosso cliente
+            Dados.AdicionarCliente(cliente);
+            return RedirectToAction("AdicionarCliente");
         }
     }
 }
---
+++ asp19/asp19/Models/Dados.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp19.Models
+{
+    public static class Dados
+    {
+        //Dados da aplicação(simulando um database)
+        private static List<Cliente> ListaCliente = new List<Cliente>();
+
+        public static List<Cliente> TodosClientes()
+        {
+            //todos os clientes da apliacação
+            return ListaCliente;
+        }
+        //Metodo pérmite guardar os clientes na database
+        public static void AdicionarCliente(Cliente clienteTemp)
+        {
+            //buscar o id disponível DO CLIENTE sendo que vamos pegar o ultimo id e acrescentar e somar mais 1 para não repetir o id
+            int id = 0;
+            if (ListaCliente.Count != 0)
+            {
+                id = ListaCliente.Last<Cliente>().Id + 1;
+            }
+            //guardar os dados do novo cliente
+            clienteTemp.Id = id;
+            ListaCliente.Add(clienteTemp);           
+        }
+    }
+}
